1a997dc90b224b7adba6ecee0f4c2117a42d1384
Dominic Tremblay <dominictremblay24@gmail.com>
2019-08-05 12:20:01 -0400

    added readme and made the components initialized

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.scss';
 import NewTweet from './NewTweet';
+import Nav from './Nav';
+import Header from './Header';
+import TweetList from './TweetList';
 
 function App() {
   return (
     <main class="container">
       <Nav />
-      <Header />
+      <Header name="SpongeBob" profileImg="/images/profile-hex.png" />
       <NewTweet />
-      <TweetList />
+      <TweetList tweets={[]} />
     </main>
   );
 }
