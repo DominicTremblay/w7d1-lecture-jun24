32824ae232c7952244f09f2c1b9f8d6be7904f3a
Dominic Tremblay <dominictremblay24@gmail.com>
2019-08-06 09:10:55 -0400

    added comments in new tweet

--- src/NewTweet.js
+++ src/NewTweet.js
@@ -2,6 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './NewTweet.scss';
 
+// 1. Create a controlled component on the textarea
+// 2. Update the counter as we type
+// 3. Create a validation before submitting
+// 4. submit tweet will send the tweet content via submitTweet and reset the textarea
+// 5. create a new tweet and add it to the list of existing tweets
+
 const NewTweet = ({ submitTweet }) => {
   const handleSubmit = event => {
     event.preventDefault();
