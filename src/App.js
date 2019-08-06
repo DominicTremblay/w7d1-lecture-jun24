a5c0380ec9aeddcf8fdd4c29b07f237d03608b6a
Dominic Tremblay <dominictremblay24@gmail.com>
2019-08-06 12:30:04 -0400

    demo with useState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import NewTweet from './NewTweet';
 import Nav from './Nav';
 import Header from './Header';
 import TweetList from './TweetList';
 
+import { tweetData } from './lib/tweetData';
+
+// 1. We need to track the state of tweets - Add the build-in tweets
+// 2. Update the list of tweets whenever there is a new tweet
+// 3. Make a request to the backend API to get a list of tweets
+
 function App() {
+  const [tweets, setTweets] = useState(tweetData);
+
+  const submitTweet = content => {
+    const newTweet = {
+      user: {
+        name: 'SpongeBob',
+        avatars: 'https://i.imgur.com/nlhLi3I.png',
+        handle: '@bob',
+      },
+      content: {
+        text: content,
+      },
+      created_at: Date.now(),
+    };
+
+    setTweets([newTweet, ...tweets]);
+  };
+
   return (
     <main class="container">
       <Nav />
       <Header name="SpongeBob" profileImg="/images/profile-hex.png" />
-      <NewTweet />
-      <TweetList tweets={[]} />
+      <NewTweet submitTweet={submitTweet} />
+      <TweetList tweets={tweets} />
     </main>
   );
 }
--- src/NewTweet.js
+++ src/NewTweet.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './NewTweet.scss';
 
@@ -9,24 +9,54 @@ import './NewTweet.scss';
 // 5. create a new tweet and add it to the list of existing tweets
 
 const NewTweet = ({ submitTweet }) => {
+  const MAX_COUNT = 140;
+
+  const [text, setText] = useState('');
+  const [count, setCount] = useState(MAX_COUNT);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const validateTweet = () => {
+    if (text.length <= 0) {
+      setErrorMsg('Please enter a tweet.');
+      return false;
+    }
+
+    if (text.length > MAX_COUNT) {
+      setErrorMsg(`Please enter a tweet of less than ${MAX_COUNT} characters`);
+      return false;
+    }
+
+    return true;
+  };
+
   const handleSubmit = event => {
     event.preventDefault();
-    submitTweet('tweet content');
+    if (validateTweet()) {
+      submitTweet(text);
+      setText('');
+      setCount(MAX_COUNT);
+    }
   };
 
   return (
     <section className="new-tweet">
       <header>
         <div id="error-container">
-          <h4 />
+          <h4>{errorMsg}</h4>
         </div>
       </header>
 
       <form onSubmit={handleSubmit}>
-        <textarea name="text" placeholder="What are you humming about?" />
+        <textarea
+          name="text"
+          placeholder="What are you humming about?"
+          onChange={event => setText(event.target.value)}
+          value={text}
+          onKeyUp={() => setCount(MAX_COUNT - text.length)}
+        />
         <footer>
           <input className="btn-new-tweet" type="submit" value="Tweet" />
-          <span className="counter">140</span>
+          <span className="counter">{count}</span>
         </footer>
       </form>
     </section>
