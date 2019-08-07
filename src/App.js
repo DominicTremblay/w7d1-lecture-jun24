ff8574c81c078546d07105665a1eed1b6a3b5fd6
Dominic Tremblay <dominictremblay24@gmail.com>
2019-08-07 12:12:19 -0400

    added the axios request with a useEffect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import NewTweet from './NewTweet';
 import Nav from './Nav';
 import Header from './Header';
 import TweetList from './TweetList';
+import axios from 'axios';
 
-import { tweetData } from './lib/tweetData';
+// import { tweetData } from './lib/tweetData';
 
 // 1. We need to track the state of tweets - Add the build-in tweets
 // 2. Update the list of tweets whenever there is a new tweet
 // 3. Make a request to the backend API to get a list of tweets
 
 function App() {
-  const [tweets, setTweets] = useState(tweetData);
+  const [tweets, setTweets] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  console.log('-'.repeat(20));
+  console.log(tweets);
+
+  // Creating a side effect: making a request
+  useEffect(() => {
+    setLoading(true);
+
+    axios
+      .get('/tweets')
+      .then(({ data }) => {
+        setTweets(data);
+        setLoading(false);
+        setError(null);
+      })
+      .catch(err => {
+        console.log(err);
+        setError(`Error, ${err.message}`);
+      });
+  }, []);
 
   const submitTweet = content => {
     const newTweet = {
@@ -30,8 +53,20 @@ function App() {
     setTweets([newTweet, ...tweets]);
   };
 
+  if (loading) {
+    return (
+      <main className="container">
+        <Nav />
+
+        <div className="loading">
+          <h1>Loading tweets...</h1>
+        </div>
+      </main>
+    );
+  }
+
   return (
-    <main class="container">
+    <main className="container">
       <Nav />
       <Header name="SpongeBob" profileImg="/images/profile-hex.png" />
       <NewTweet submitTweet={submitTweet} />
